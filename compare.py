96212a7ab1d0a7d6f6626c7a5620426dcd120fd3
Doglum <danielgmorgan99@gmail.com>
2021-03-31 16:07:45 +0100

    Created duel and tournament functions

--- compare.py
+++ compare.py
@@ -1,11 +1,12 @@
 import poker, cfr
 import matplotlib.pyplot as plt
 from copy import deepcopy
+from itertools import combinations
 
 global figureNum
 figureNum = 0
 def plotLine(values,title,x="Round No.",y="Advantage"):
-    """Plots a line"""
+    """Plots a line chart with pyplot"""
     global figureNum
     figureNum+=1
     plt.figure(figureNum)
@@ -31,62 +32,13 @@ def getMBBValues(vals,bb = 20):
         cumValue += vals[i]
         mbb.append(((cumValue/bb)*1000)/(i+1))
     return mbb
-    
 
-if __name__ == "__main__":
-    from cfr import Sets,InfoSet
-    #creates deck & players
-    deck = poker.Card.getDeck()
-    playerList = poker.Player.getPlayerList(2,500)
+def duel(player1,player2,rounds = 10000,printing = True):
+    """Simulates rounds of poker between two AIs and returns their winnings
+    and losses, will print an update message at intervals if printing is true"""
+    playerList[0] = player1
+    playerList[1] = player2
     
-    #initialises player objects for AIs
-    raisePlayer = poker.Player(500)
-    raisePlayer.AI = poker.raiseBot
-    
-    randomPlayer = poker.Player(500)
-    randomPlayer.AI = poker.randomBot
-    
-    
-    #Abstraction level 1, non forgetful (b - basic)
-    bPlayer = poker.Player(500)
-    bPlayer.info, bitr = cfr.getMostRecentSave("Abstract1")
-    bPlayer.AI = poker.CFRIntelligence
-    bPlayer.absLevel = 1
-    
-    #Abstraction level 2, non forgetful (a - advanced)
-    aPlayer = poker.Player(500)
-    aPlayer.info, aitr = cfr.getMostRecentSave("Abstract2")
-    aPlayer.AI = poker.CFRIntelligence
-    aPlayer.absLevel = 2
-    
-    #Abstraction level 1, forgetful (bf -basic forgetful)
-    bfPlayer = poker.Player(500)
-    bfPlayer.info, bfItr = cfr.getMostRecentSave("ForgetfulAbstract1")
-    bfPlayer.AI = poker.CFRIntelligence
-    bfPlayer.absLevel = 1
-    bfPlayer.forgetful = True
-    
-    #Abstraction level 2, forgetful (af -advanced forgetful)
-    afPlayer = poker.Player(500)
-    afPlayer.info, afItr = cfr.getMostRecentSave("ForgetfulAbstract2")
-    afPlayer.AI = poker.CFRIntelligence
-    afPlayer.absLevel = 2
-    afPlayer.forgetful = True
-    
-    #Probabilistic, forgetful (p - probabilistic)
-    pPlayer = poker.Player(500)
-    pPlayer.info,pItr = cfr.getMostRecentSave("ForgetfulProbabilistic")
-    pPlayer.AI = poker.CFRIntelligence
-    pPlayer.forgetful = True
-    pPlayer.probabilistic = True
-    
-    #CONFIGURE as appropriate, player 0 goes first
-    playerList[0] = bfPlayer
-    playerList[1] = afPlayer
-    
-    graphTitle = "Player 1 winnings"
-    
-    rounds = 10000
     updateInterval = rounds/10
     
     #setup
@@ -113,12 +65,96 @@ if __name__ == "__main__":
             p.chips = 500
             
         if i % updateInterval == 0:
-            print("Reached",i,"Iterations")
-    
-    
-    
-    
-    plotLine(getMBBValues(p1Winnings)[500:],graphTitle)
-    print((sum(p1Winnings)/(bigBlind/1000)/rounds),"mbb/g")
-    plotLine(getCumulativeValues(p1Winnings),graphTitle)
+            if printing: print("Reached",i,"Iterations")
+            
+    return p1Winnings, p2Winnings
+
+def tournament(players,rounds = 100):
+    """Performs 1 duel between each player in players and 
+    returns 2d winnings list"""
+    winnings = [[0 for i in range(len(players))] for i in range(len(players))]
+    for pair in combinations(players,2):
+        i1 = players.index(pair[0])
+        i2 = players.index(pair[1])
+        win1,win2 = duel(pair[0],pair[1],rounds,printing=False)
+        winnings[i1][i2] = sum(win1)/(20/1000)/rounds
+        winnings[i2][i1] = sum(win2)/(20/1000)/rounds
         
+    return winnings
+    
+    
+    
+    
+
+if __name__ == "__main__":
+    from cfr import Sets,InfoSet
+    #creates deck & players
+    deck = poker.Card.getDeck()
+    playerList = poker.Player.getPlayerList(2,500)
+    AIList = []
+    
+    #initialises player objects for AIs
+    raisePlayer = poker.Player(500)
+    raisePlayer.AI = poker.raiseBot
+    AIList.append(raisePlayer)
+    
+    randomPlayer = poker.Player(500)
+    randomPlayer.AI = poker.randomBot
+    AIList.append(randomPlayer)
+    
+    #Abstraction level 1, non forgetful (b - basic)
+    bPlayer = poker.Player(500)
+    bPlayer.info, bitr = cfr.getMostRecentSave("Abstract1")
+    bPlayer.AI = poker.CFRIntelligence
+    bPlayer.absLevel = 1
+    AIList.append(bPlayer)
+    
+    #Abstraction level 2, non forgetful (a - advanced)
+    aPlayer = poker.Player(500)
+    aPlayer.info, aitr = cfr.getMostRecentSave("Abstract2")
+    aPlayer.AI = poker.CFRIntelligence
+    aPlayer.absLevel = 2
+    AIList.append(aPlayer)
+    
+    #Abstraction level 1, forgetful (bf -basic forgetful)
+    bfPlayer = poker.Player(500)
+    bfPlayer.info, bfItr = cfr.getMostRecentSave("ForgetfulAbstract1")
+    bfPlayer.AI = poker.CFRIntelligence
+    bfPlayer.absLevel = 1
+    bfPlayer.forgetful = True
+    AIList.append(bfPlayer)
+    
+    #Abstraction level 2, forgetful (af -advanced forgetful)
+    afPlayer = poker.Player(500)
+    afPlayer.info, afItr = cfr.getMostRecentSave("ForgetfulAbstract2")
+    afPlayer.AI = poker.CFRIntelligence
+    afPlayer.absLevel = 2
+    afPlayer.forgetful = True
+    AIList.append(afPlayer)
+    
+    #Probabilistic, forgetful (p - probabilistic)
+    pPlayer = poker.Player(500)
+    pPlayer.info,pItr = cfr.getMostRecentSave("ForgetfulProbabilistic")
+    pPlayer.AI = poker.CFRIntelligence
+    pPlayer.forgetful = True
+    pPlayer.probabilistic = True
+    #AIList.append(pPlayer)
+    
+    #CONFIGURE as appropriate, player 0 goes first
+    playerList[0] = bfPlayer
+    playerList[1] = afPlayer
+    
+    graphTitle = "Player 1 winnings"
+    
+    rounds = 10000
+    
+    
+    winnings = tournament(AIList,rounds)
+    
+    
+    #p1Winnings, p2Winnings = duel(playerList[0],playerList[1],rounds)
+    """
+    plotLine(getMBBValues(p1Winnings)[500:],graphTitle)
+    print((sum(p1Winnings)/(20/1000)/rounds),"mbb/g")
+    plotLine(getCumulativeValues(p1Winnings),graphTitle)
+    """
